acc45cf548a27b15e06ffc9d070ddc1a7813a836
YanaLifanova <janali.sqa@gmail.com>
2025-04-13 11:35:03 -0500

    first test

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+
+describe('description of the suite', () => {
+
+    it("description of the test", ()=>{
+        cy.visit("https://www.automationexercise.com/test_cases");
+        cy.get('a[href="/"]').should('have.css', 'color', 'rgb(66, 139, 202)');
+
+    });
+});
