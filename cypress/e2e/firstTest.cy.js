acfa7a5a25127242ba2d9d5b9362e673149d79f9
YanaLifanova <janali.sqa@gmail.com>
2025-04-14 11:55:24 -0500

    add README

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -6,7 +6,6 @@ describe('description of the suite', () => {
     it("description of the test", ()=>{
         cy.visit("https://www.automationexercise.com/test_cases");
         cy.get('a[href="/"]').should('have.css', 'color', 'rgb(66, 139, 202)');
-
         
     });
 });
